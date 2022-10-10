81b5769e2ea2cb53c057327f7e8ccd8b1ada2751
mike591 <mikemach123@gmail.com>
2022-10-10 16:08:25 -0700

    added start game function

--- apps/web/src/components/Setup.tsx
+++ apps/web/src/components/Setup.tsx
@@ -4,21 +4,66 @@ import { FIRST_TO_POINTS_GAME_MODE, ROUNDS_GAME_MODE } from "utils/constants";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { GameState } from "hooks/useGame";
 import UserCard from "./UserCard";
+import axios from "axios";
+import getGameKeyFromLocation from "utils/getGameKeyFromLocation";
 import { useState } from "react";
 import useUser from "hooks/useUser";
 
 const { Option } = Select;
+const DEFAULT_NUM_POINTS = 21;
+
+type GameMode = typeof ROUNDS_GAME_MODE | typeof FIRST_TO_POINTS_GAME_MODE;
+
+interface StartGameProps {
+  gameMode: GameMode;
+  game: GameState["game"];
+  numPoints: number;
+}
+const useStartGame = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<unknown>(null);
+
+  const { id } = useUser();
+
+  async function startGame({ gameMode, game, numPoints }: StartGameProps) {
+    setLoading(true);
+    try {
+      const url = `http://${process.env.REACT_APP_API_DOMAIN}/game/${game?.gameKey}/start`;
+      await axios({
+        method: "POST",
+        url,
+        headers: {
+          user_id: id,
+        },
+        data: {
+          gameMode,
+          numPoints,
+        },
+      });
+    } catch (err: unknown) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return {
+    loading,
+    error,
+    startGame,
+  };
+};
 
 interface ControlsProps {
   game: GameState["game"];
 }
 const Controls = ({ game }: ControlsProps) => {
-  const [gameMode, setGameMode] = useState(FIRST_TO_POINTS_GAME_MODE);
+  const [gameMode, setGameMode] = useState<GameMode>(FIRST_TO_POINTS_GAME_MODE);
   const [copied, setCopied] = useState(false);
 
-  function handleSetGameMode(
-    value: typeof FIRST_TO_POINTS_GAME_MODE | typeof ROUNDS_GAME_MODE
-  ) {
+  const { startGame, loading } = useStartGame();
+
+  function handleSetGameMode(value: GameMode) {
     setGameMode(value);
   }
 
@@ -29,9 +74,8 @@ const Controls = ({ game }: ControlsProps) => {
     }, 2000);
   }
 
-  function handleStart() {
-    //TODO: setup start game
-    console.log("start");
+  async function handleStart() {
+    await startGame({ gameMode, game, numPoints: DEFAULT_NUM_POINTS });
   }
 
   return (
@@ -45,8 +89,8 @@ const Controls = ({ game }: ControlsProps) => {
             First to points
           </Option>
         </Select>
-        <Select defaultValue="21" disabled>
-          <Option value="21">21</Option>
+        <Select defaultValue={DEFAULT_NUM_POINTS} disabled>
+          <Option value={DEFAULT_NUM_POINTS}>{DEFAULT_NUM_POINTS}</Option>
         </Select>
       </div>
       <div className="flex flex-col gap-2 items-center">
